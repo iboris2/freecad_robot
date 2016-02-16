05edabc199aa00bbea4c1ac43263d82348583ba7
iboris <iboris@free.fr>
2016-02-16 19:34:16 +0100

    clamp with hole
    Signed-off-by: iboris <iboris@free.fr>

--- export/hole_center.py
+++ export/hole_center.py
@@ -7,30 +7,30 @@ l_size=5
 doc = parse(os.path.abspath(sys.argv[1]))
 root = doc.getroot()
 
-wr = root.find('.//{http://www.w3.org/2000/svg}g')
+for wr in root.findall('.//g'):
+	for node in wr.findall('.//circle'):
+		print node
+		x = float(node.attrib.get('cx'))
+		y = float(node.attrib.get('cy'))
+		#node.attrib["style"]='stroke-width:0.35;stroke-miterlimit:4;stroke-dasharray:none;fill:none'
+		l1 = Element('line')
+		stroke-width="0.25 px"
+		l1.attrib['x1']= str(x - l_size)
+		l1.attrib['y1']= str(y)
+		l1.attrib['x2']= str(x + l_size)
+		l1.attrib['y2']= str(y)
+		#l1.attrib['style']="stroke-width:0.35"
 
-for node in root.findall('.//{http://www.w3.org/2000/svg}circle'):
-  x = float(node.attrib.get('cx'))
-  y = float(node.attrib.get('cy'))
-  node.attrib["style"]='stroke-width:0.35;stroke-miterlimit:4;stroke-dasharray:none;fill:none'
-  l1 = Element('ns0:line')
-  l1.attrib['stroke']="black"
-  l1.attrib['x1']= str(x - l_size)
-  l1.attrib['y1']= str(y)
-  l1.attrib['x2']= str(x + l_size)
-  l1.attrib['y2']= str(y)
-  l1.attrib['style']="stroke-width:0.35"
-
-  l2 = Element('ns0:line')
-  l2.attrib['stroke']="black"
-  l2.attrib['x1']= str(x)
-  l2.attrib['y1']= str(y - l_size)
-  l2.attrib['x2']= str(x)
-  l2.attrib['y2']= str(y + l_size)
-  l2.attrib['style']="stroke-width:0.35"
-  
-  wr.append(l1)
-  wr.append(l2)
+		l2 = Element('line')
+		#l2.attrib['stroke']="black"
+		l2.attrib['x1']= str(x)
+		l2.attrib['y1']= str(y - l_size)
+		l2.attrib['x2']= str(x)
+		l2.attrib['y2']= str(y + l_size)
+		#l2.attrib['style']="stroke-width:0.35"
+			  
+		wr.append(l1)
+		wr.append(l2)
 
 print os.path.abspath(sys.argv[2])
 doc.write(os.path.abspath(sys.argv[2]))
--- mod/svgexporter/module1.py
+++ mod/svgexporter/module1.py
@@ -41,17 +41,62 @@ class MyTool:
 		# do something here...
 		svg = []
 		for obj in FreeCADGui.Selection.getSelectionEx():
+			#obj.Object.Placement = FreeCAD.Placement()
 			for face in obj.SubObjects:
 				f = face.copy()
 				box = f.BoundBox
 				f.translate(box.Center * -1)
 				#v = FreeCAD.Vector(box.XLength,box.YLength,box.ZLength)
 				#diag = v.cross(f.normalAt(0,0))
-				svg.append(Drawing.projectToSVG(f,f.normalAt(0,0)))
+				svg.append(Drawing.projectToSVG(f,f.Surface.Axis))
 				del f
+				
+		#FreeCAD.Console.PrintMessage("placement " + str(obj) + str(pl) + "\n")
+		#orient = face.Surface.Axis
+		#FreeCAD.Console.PrintMessage("orient " + str(orient) + "\n")
+		#r = FreeCAD.Rotation(FreeCAD.Vector(0,0,1),orient)
+		#pl.Rotation = r
+		#obj.Object.Placement = pl
+				
 		data = ""
 		for s in svg:
-			data += s 
-		open("/tmp/svg.svg","w").write("<svg>"+data+"</svg>")
+			data += s
+		data = "<svg>"+data+"</svg>"
+		import sys
+		import os
+		from xml.etree.ElementTree import fromstring, Element, ElementTree
+
+		l_size=5
+
+		root = fromstring(data)
+		et = ElementTree(root)
+
+		for wr in root.findall('.//g'):
+			wr.attrib['stroke-width']="0.30 px"
+			for node in wr.findall('.//circle'):
+				print node
+				x = float(node.attrib.get('cx'))
+				y = float(node.attrib.get('cy'))
+				#node.attrib["style"]='stroke-width:0.35;stroke-miterlimit:4;stroke-dasharray:none;fill:none'
+				l1 = Element('line')
+				l1.attrib['x1']= str(x - l_size)
+				l1.attrib['y1']= str(y)
+				l1.attrib['x2']= str(x + l_size)
+				l1.attrib['y2']= str(y)
+				#l1.attrib['style']="stroke-width:0.35"
+
+				l2 = Element('line')
+				#l2.attrib['stroke']="black"
+				l2.attrib['x1']= str(x)
+				l2.attrib['y1']= str(y - l_size)
+				l2.attrib['x2']= str(x)
+				l2.attrib['y2']= str(y + l_size)
+				#l2.attrib['style']="stroke-width:0.35"
+					  
+				wr.append(l1)
+				wr.append(l2)
+				
+		et.write("/tmp/svg.svg")
+
 		
 FreeCADGui.addCommand('MyCommand1',MyTool())
